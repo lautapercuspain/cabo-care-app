fb8f9ef32831317be2a401aaafba523e559b3624
lautapercuspain <lautarogruss@gmail.com>
2021-01-29 12:22:43 -0700

    Poppins

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,8 +34,12 @@ export default class MyDocument extends Document {
             rel="icon"
             href="https://is3-ssl.mzstatic.com/image/thumb/Purple114/v4/64/90/b5/6490b502-d6e1-2499-515c-af3226625075/AppIcon-1x_U007emarketing-0-5-0-0-85-220.png/200x200bb.png"
           />
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins"
+            rel="stylesheet"
+          />
         </Head>
-        <body>
+        <body style={{fontFamily: 'Poppins'}}>
           <Main />
           <NextScript />
         </body>
